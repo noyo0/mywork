032e8e2d4a0afc777adf7e6475a0b835fb9adde8
Noyo0 <norbert.antaloutlook.com>
2023-02-16 20:42:25 +0000

    w04 updtae

--- Week04/isEven.py
+++ Week04/isEven.py
@@ -2,4 +2,4 @@ number = int(input("enter an integer:"))
 if (number % 2) == 0:
     print (f"{number} is an even number")
 else:
-    print(f"{number} is an odd number")
+    print(f"{number} is an odd number") 
