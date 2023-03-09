7ebf4992d5fdf320578040870136599af6ee7a09
Noyo0 <norbert.antaloutlook.com>
2023-03-09 23:07:13 +0000

    messing

--- Week07/messwithFiles.py
+++ Week07/messwithFiles.py
@@ -1,17 +1,7 @@
 # messing with files
 #author: Norbert Antal
-FILENAME = "data.txt"
-"""
+FILENAME = "2BR02B_K.Vonnegut.txt"
+
 with open(FILENAME,"r") as f:
     for data in f:
         print(data.strip())
-"""
-with open("data2.txt", "w+") as f:
-    f.write("data2 text\n")
-    f.write("\ndata2 line 2\n")
-    f.seek(0)
-
-    data = f.read()
-    print(data)
-
-print("done")
