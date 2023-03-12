c05df4dcc55ded3cbf4061f49788995d894b69a6
Noyo0 <norbert.antaloutlook.com>
2023-03-12 17:30:56 +0000

    email domains from csv + prog run counter

---
+++ Week07/persistent.py
@@ -0,0 +1,23 @@
+import os
+FILENAME="count.txt"
+
+def fn_readnumber():
+    try:
+        with open(FILENAME) as f:
+            num=int(f.read())
+            fn_writenumber(num+1)
+            return(num)
+    except IOError:
+        return(0) 
+
+def fn_writenumber(number):
+    with open(FILENAME, "wt") as f:
+        f.write(str(number))
+
+if not os.path.isfile(FILENAME):
+    print("file doesn\'t exist")
+    fn_writenumber(0)
+    
+num=fn_readnumber()
+print(f"we have run this program {num} times")
+        
