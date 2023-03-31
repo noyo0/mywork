85cd1e6f4f5e3e7bd70972fa721ce388e17acc21
Noyo0 <norbert.antaloutlook.com>
2023-03-31 22:05:30 +0100

    module demo

---
+++ Week10/personsmodule.py
@@ -0,0 +1,20 @@
+# demo of a module
+# Author: Norbert Antal
+import datetime as dt
+def gethealthdata(person):
+    print("get health data: ", person['firstname'])
+
+def displayperson(person):
+    print(person)
+
+if __name__=='__main__':
+    person1 = {
+        'firstname' : 'andrew',
+        'lastname' : 'beatty',
+        'dob' : dt.date(210,1,1),
+        'height' : 180,
+        'width' : 100
+    }
+
+displayperson(person1)
+gethealthdata(person1)
